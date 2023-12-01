d0fa9e78bcebf54794c20f009aeb75e708213c9d
MuhZaidanRamdhan <muhammadzaidanramdhan509@gmail.com>
2023-12-01 19:26:19 +0700

    task

--- pertemuan09/app.js
+++ pertemuan09/app.js
@@ -1,10 +1,14 @@
-const {index, store} = require('./fruitsController.js');
+const {index, store, update, destroy} = require('./fruitsController.js');
 
 const main = () => {
     console.log('Menampilkan seluruh buah  ');
     index();
     console.log('\n');
     store("strawberry");
+    console.log('\n');
+    update(1, 'blueberry');
+    console.log('\n');
+    destroy(1);
 };
 
 main();
